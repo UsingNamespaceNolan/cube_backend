f53694b09b2ac110b8e705bb36089b8e68302b3e
UsingNamespaceNolan <nolan.johnston335@gmail.com>
2024-11-16 21:29:02 -0500

    add database migration updates to the local run script

--- run_local.sh
+++ run_local.sh
@@ -1,1 +1,5 @@
+python manage.py makemigrations --settings=cube_project.settings.base &&
+python manage.py migrate --settings=cube_project.settings.base &&
+python manage.py makemigrations cube_app --settings=cube_project.settings.base &&
+python manage.py migrate cube_app --settings=cube_project.settings.base &&
 python manage.py runserver 8001 --settings=cube_project.settings.local
